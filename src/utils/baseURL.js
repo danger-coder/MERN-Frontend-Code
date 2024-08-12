ead73f479054d54c77c264251f46e7a8648847fe
karan <karanchy95@gmail.com>
2024-08-12 15:30:47 +0545

    Frontend code

---
+++ src/utils/baseURL.js
@@ -0,0 +1,4 @@
+const baseURL = "http://localhost:2030/api/v1";
+// const baseURL = "https://nodejs-ecommerce-api.onrender.com/api/v1";
+// const baseURL = "http://localhost:7000/api/v1";
+export default baseURL;
